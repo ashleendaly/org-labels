185972b33d0e24f7fe58afb0b487b3843d2f43df
Ashleen Daly <Ashleen.Daly@res-group.com>
2023-06-07 18:05:53 +0100

    added function to import labels from a csv file

--- label-automator-async.py
+++ label-automator-async.py
@@ -1,14 +1,27 @@
 import asyncio
 import httpx
 from dotenv import load_dotenv
+import csv
 
 load_dotenv()
 
-labels = {
-        "name": "retrospective",
-        "color": "d8278d",
-        "description": "This issue is to be discussed in the next retrospective."
-    }
+def import_labels_from_csv(file_name):
+    labels = []
+
+    with open(file_name, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for i, row in enumerate(csv_reader):
+            if i == 0:
+                continue
+            else:
+                labels.append({
+                    "name": row[0],
+                    "description": row[1],
+                    "color": row[2]
+                })
+            
+    
+    return labels
 
 def gather_input(item_to_gather):
     gathered_input = input(f"Enter your {item_to_gather}: ")
@@ -33,19 +46,27 @@ async def get_repositories(organisation, username, token):
         response = await client.get(f"https://api.github.com/orgs/{organisation}/repos", auth=(username, token))
     return response.json()
 
-async def add_label_to_repo(repo, username, token):
+async def add_labels_to_repo(repo, username, token, label):
     async with httpx.AsyncClient() as client:
-        response = await client.post(url=f"https://api.github.com/repos/{repo['owner']['login']}/{repo['name']}/labels", auth=(username, token), json=labels)
+        response = await client.post(url=f"https://api.github.com/repos/{repo['owner']['login']}/{repo['name']}/labels", auth=(username, token), json=label)
         print(f"Added label to {repo['name']}")
         return response
+        
 
 async def main():
+
     username = gather_input("GitHub username")
     token = gather_input("GitHub token")
     organisation = gather_input("GitHub organisation")
+    file_name = gather_input("CSV file name")
+
+
+    labels = import_labels_from_csv(file_name)
 
     all_repositories = await get_repositories(organisation, username, token)
-    tasks = [add_label_to_repo(repo, username, token) for repo in all_repositories]
+
+    tasks = [add_labels_to_repo(repo, username, token, label) for label in labels for repo in all_repositories]
+
     await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
